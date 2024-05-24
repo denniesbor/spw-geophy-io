99204cd99768cb6e22af1a7bf5773e1aa22b4e84
dennies bor <borkiprono.com>
2024-05-24 01:18:14 -0400

    Load data from github gist

--- data-col-api/main.js
+++ data-col-api/main.js
@@ -41,8 +41,10 @@ function initMap() {
 
 // Fetch data from JSON file
 async function fetchData() {
+  let url =
+    "https://gist.githubusercontent.com/denniesbor/81dfc2b05d3c7ee0f02dfc20ec15dce8/raw/a9c6c3b0f9fdc604d4884ded950b1d5035c90e22/tm_ss_df_gt_300v.json";
   try {
-    const response = await fetch("./data/tm_ss_df_gt_300v.json");
+    const response = await fetch(url);
     data = await response.json();
     console.log("Data fetched successfully:"); // Debugging step
     if (Array.isArray(data)) {
@@ -57,8 +59,10 @@ async function fetchData() {
 
 // Fetch GeoJSON for transmission lines
 async function fetchTransmissionLines() {
+  let url =
+    "https://gist.githubusercontent.com/denniesbor/f55327cd9a7ba2c7da2725c5b03b17f0/raw/ece03a294a758201597da9c80a50759726425b09/tm_lines_within_ferc.geojson";
   try {
-    const response = await fetch("./data/tm_lines_within_ferc.geojson");
+    const response = await fetch(url);
     const geoJsonData = await response.json();
     transmissionLinesLayer = new google.maps.Data();
     transmissionLinesLayer.addGeoJson(geoJsonData);
